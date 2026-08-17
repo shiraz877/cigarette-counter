0104c6f8caf627406e69e22ec94331ca90141eaf
shiraz877 <gm6564@myamu.ac.in>
2026-08-17 12:53:45 +0530

    Initial Commit

---
+++ CigaretteCouter/CigaretteCouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CigaretteCouterApp.swift
+//  CigaretteCouter
+//
+//  Created by Shiraz on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CigaretteCouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
